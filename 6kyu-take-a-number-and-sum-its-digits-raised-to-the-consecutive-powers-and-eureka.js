f817c511cb718475f3d94b63e2a8d3bc85fe9689
Thomas Chun <tchun2055@gmail.com>
2019-06-10 14:59:31 -0700

    completed

--- 6kyu-take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-eureka.js
+++ 6kyu-take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-eureka.js
@@ -23,12 +23,23 @@
 // Enjoy it!!
 
 function sumDigPow(a, b) {
-  // Your code here
+  let result = [];
+  for (let i = a; i <= b; i++) {
+    let numArr = String(i).split('');
+    let total = 0;
+    for (let j = 0; j < numArr.length; j++) {
+      total += Math.pow(numArr[j], j+1);
+    }
+    if (total === i) {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
-console.log(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-console.log(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]);
-console.log(sumDigPow(10, 100),  [89]);
+// console.log(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+// console.log(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]);
+// console.log(sumDigPow(10, 100),  [89]);
 console.log(sumDigPow(90, 100), []);
 console.log(sumDigPow(90, 150), [135]);
 console.log(sumDigPow(50, 150), [89, 135]);
