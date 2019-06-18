159f108a250fd8593477eb228ddff86e12db774b
Thomas Chun <tchun2055@gmail.com>
2019-06-17 20:51:07 -0700

    compelted

---
+++ 6kyu-grouped-by-commas.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Finish the solution so that it takes an input n (integer) and returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
+
+// Assume: 0 <= n < 2147483647
+
+// Examples
+//        1  ->           "1"
+//       10  ->          "10"
+//      100  ->         "100"
+//     1000  ->       "1,000"
+//    10000  ->      "10,000"
+//   100000  ->     "100,000"
+//  1000000  ->   "1,000,000"
+// 35235235  ->  "35,235,235"
+
+function groupByCommas(n) {
+  let result = [];
+  n = String(n).split('').reverse();
+  console.log(n);
+  if (n.length > 3) {
+    console.log('entered > 3');
+    for (let i = 0; i < n.length; i++) {
+      if (i % 2 === 0) {
+        console.log('enter comma');
+        result.push(`${n[i]}`);
+      } else {
+        console.log('entered else statement');
+        result.push(n[i]);
+      }
+    } 
+  }
+  console.log('result', result);
+  return result.reverse().join('');
+}
+
+// console.log(groupByCommas(1), '1');
+// console.log(groupByCommas(10), '10');
+// console.log(groupByCommas(100), '100');
+console.log(groupByCommas(1000), '1,000');
+console.log(groupByCommas(10000), '10,000');
+// console.log(groupByCommas(100000), '100,000');
+// console.log(groupByCommas(1000000), '1,000,000');
+// console.log(groupByCommas(35235235), '35,235,235');
